b58402c9356697775cafbcb19424b35c5198946f
John Doe <johndoe@email.com>
2024-01-24 17:09:25 +0530

    klll

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import {HashRouter, Routes, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route, BrowserRouter} from 'react-router-dom';
 import Home from './pages/home';
 import Resume from './pages/resume';
 import About from './pages/about';
 
 function App() {
   return (
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route index element = {<Home />} />
         <Route path='/' exact element = {<Home />} />
         <Route exact path='/resume' element = {<Resume />} />
         <Route path='/about' element = {<About />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
