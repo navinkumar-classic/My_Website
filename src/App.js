f61f122faf74ea24a1eb269914ed9df6f44b86d4
John Doe <johndoe@email.com>
2024-01-24 17:11:36 +0530

    hjhj

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route, BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/home';
 import Resume from './pages/resume';
 import About from './pages/about';
