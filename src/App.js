cd3823c87264f7a8f025dda7a4ee2e7258fe677f
John Doe <johndoe@email.com>
2024-01-24 16:49:40 +0530

    pleaseeee

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import About from './pages/about';
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={process.env.PUBLIC_URL + '/My_Website'}>
       <Routes>
         <Route index element = {<Home />} />
         <Route path='/' exact element = {<Home />} />
-        <Route exact path='/My_Website/resume' element = {<Resume />} />
+        <Route exact path='/resume' element = {<Resume />} />
         <Route path='/about' element = {<About />} />
       </Routes>
     </BrowserRouter>
